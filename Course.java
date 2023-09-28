531f23eea6b2687106a658e55fa8e7fe88df211b
Hasan6634 <138626695+Hasan6634@users.noreply.github.com>
2023-09-28 15:12:46 +0300

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,27 @@
+class Course {
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+    int sNote;
+
+    Course(String name, String code, String prefix) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        int note = 0;
+        int sNote=0;
+    }
+
+    void addTeacher(Teacher teacher) {
+        if (teacher.branch.equals(this.prefix)) {
+            this.teacher = teacher;
+        }else
+            System.out.println("Öğretmen ve ders bölümleri uyuşmuyor");
+    }
+
+    void printTeacherInfo() {
+        this.teacher.print();
+    }
+}
